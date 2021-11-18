aea3be0f80551416c21548b8a27105952a45897b
Pollab Ahmed <pollab17@gmail.com>
2021-11-18 08:01:12 +0600

    upload

--- src/test/java/RunTest.java
+++ src/test/java/RunTest.java
@@ -5,26 +5,32 @@ import java.io.IOException;
 
 public class RunTest {
     Customer customer = new Customer();
+
     @Test
     public void login() throws ConfigurationException, IOException {
         customer.CallingLoginApi();
     }
+
     @Test
     public void customerList() throws IOException {
         customer.CustomerList();
     }
+
     @Test
     public void createCustomer() throws IOException {
         customer.CreateCustomer();
     }
+
     @Test
     public void searchCustomer() throws IOException {
         customer.SearchCustomer();
     }
+
     @Test
     public void updateCustomer() throws IOException {
         customer.UpdateCustomer();
     }
+
     @Test
     public void deleteCustomer() throws IOException {
         customer.DeleteCustomer();
